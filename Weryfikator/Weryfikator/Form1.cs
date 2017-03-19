accc364d03d13b4767237dc43dab64a1ae11ce86
Antah <ktamowicz@gmail.com>
2017-03-19 18:14:01 +0100

    Wyszukiwanie pliku

---
+++ Weryfikator/Weryfikator/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weryfikator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.Cancel) return;
+            textBox1.Text = openFileDialog1.FileName;
+        }
+    }
+}
