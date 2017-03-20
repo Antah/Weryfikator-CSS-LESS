5bae5a3d6d7d26dc84642632d3a4d450eea8b715
Antah <ktamowicz@gmail.com>
2017-03-20 22:41:25 +0100

    Added dictionary

--- Weryfikator/Weryfikator/LexemeEnum.cs
+++ Weryfikator/Weryfikator/LexemeEnum.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace Weryfikator
 {
@@ -38,5 +39,42 @@ namespace Weryfikator
         FUNCTION,
         SLASH,
         DOT
+    }
+
+    public static class LexemeHashTable
+    {
+        internal static Dictionary<Lexeme, String> LexemeDictinary = new Dictionary<Lexeme, String>
+        {
+            { Lexeme.OPENING_BRACE, "{" },
+            { Lexeme.CLOSING_BRACE, "}" },
+            { Lexeme.OPENING_PARENTHESIS, "(" },
+            { Lexeme.CLOSING_PARENTHESIS, ")" },
+            { Lexeme.COLON, ":" },
+            { Lexeme.SEMICOLON, ";" },
+            { Lexeme.AT, "@" },
+            { Lexeme.WHITESPACE, "" },
+            { Lexeme.TEXT, "" },
+            { Lexeme.COLOR, "" },
+            { Lexeme.NUMBER, "" },
+            { Lexeme.QUOTATION, "" },
+            { Lexeme.CLOSING_ANGLE_BRACKET, ">" },
+            { Lexeme.TILDE, "~" },
+            { Lexeme.OPEN_COMMENT, "/*" },
+            { Lexeme.CLOSE_COMMENT, "*/" },
+            { Lexeme.LINE_COMMENT, "" },
+            { Lexeme.DOUBLESLASH, "//" },
+            { Lexeme.NEW_LINE, "\n" },
+            { Lexeme.PLUS, "+" },
+            { Lexeme.MINUS, "-" },
+            { Lexeme.STAR, "*" },
+            { Lexeme.COMMA, "," },
+            { Lexeme.HASH, "#" },
+            { Lexeme.UNIT, "" },
+            { Lexeme.END, "" },
+            { Lexeme.FUNCTION, "" },
+            { Lexeme.SLASH, "/" },
+            { Lexeme.DOT, "." },
+        };
+        
     }
 }
